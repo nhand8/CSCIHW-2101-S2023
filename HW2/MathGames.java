cc39d97f46408d80552732785626e68c6050863d
Dylan Nhan <nhand2@go.stockton.edu>
2023-03-24 11:26:53 -0400

    first try

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -1,5 +1,5 @@
 package HW2;
-
+import java.lang.Math;
 
 public class MathGames {
     // 1. Create a method that will calculate the Pythagorean Theorem (https://en.wikipedia.org/wiki/Pythagorean_theorem)
@@ -12,9 +12,16 @@ public class MathGames {
     // Hypotenuse and Perpendicular and return the base.
     // Example 1: pythagorean(3,4) would return 5
 
+public static double PythagoreanTheorem(double a, double b){
+       
+       double base = Math.pow(a, 2);
+       double perpendicular = Math.pow(b, 2);
+       double cSquared = base + perpendicular;
+       double c = Math.sqrt(cSquared);
+        return c;
 
+    }
 
-    
     // 2. Create a method that will calcuate my grade in the class. You can use the grade range as follows
         // A: 100 - 90
         // B:  89 - 80
@@ -28,11 +35,26 @@ public class MathGames {
         // Hint 3: You will probably need to use the Math.ceil() method
         // Example: grade(69.3) would return a D
 
-
-
-
-
-
+public static String GradeCalculator(double gradePoint){
+        String Grade = "";
+        double roundedupGrade = Math.ceil(gradePoint);
+        if(roundedupGrade>=90){
+            return "A";
+        }
+        else if(roundedupGrade<=89 && roundedupGrade>=80){
+            return "B";
+        }
+        else if(roundedupGrade<=79 && roundedupGrade>=70){
+            return "C";
+        }
+        else if(roundedupGrade<=69 && roundedupGrade>=60){
+            return "D";
+        }
+        else if(roundedupGrade<60 && roundedupGrade>=0){
+            return "F";
+        }
+        return Grade;
+    }
     // 3. Create a method that will calculate the tip on a bill per person
     //      Method parameters: total bill, amount of people splitting the bill, percentage to tip.
     //      Note: I would like the total always rounded up to a whole number
@@ -42,12 +64,20 @@ public class MathGames {
     //      Hint 4: You will probably need to use the Math.ceil() method
     //      Example: tip(100, 4, 18) would return 5.0
 
-    
- 
-
+    public static double tipCalculator(double total, int Splitpeople, double percentTip){
+        double tip = percentTip/100;
+        Math.ceil(total);
+        double totalTip = tip * total;
+        double tipPerPerson = totalTip/Splitpeople;
+        Math.ceil(tipPerPerson);
+        return tipPerPerson;
+    }
 
     // You can either create a tester class or put your code here
      public static void main(String[] args){
+        System.out.println(MathGames.PythagoreanTheorem(6, 8));
+        
+        System.out.println(MathGames.tipCalculator(100.00, 4, 18.00));
         
     }
 }
